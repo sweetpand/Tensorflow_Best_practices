ea0a81bc8c31fc542776cad4525c3478f174809e
Zham <56076483+sweetpand@users.noreply.github.com>
2020-04-05 17:59:56 +0800

    203_variable.py
    203_variable.py

---
+++ Tensorflow_tutorial_practice/203_variable.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+var = tf.Variable(0)    # our first variable in the "global_variable" set
+
+add_operation = tf.add(var, 1)
+update_operation = tf.assign(var, add_operation)
+
+with tf.Session() as sess:
+    # once define variables, you have to initialize them by doing this
+    sess.run(tf.global_variables_initializer())
+    for _ in range(3):
+        sess.run(update_operation)
+        print(sess.run(var))
